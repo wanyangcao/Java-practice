94d83565ae2abbdaefc6607538fab504a4d41ecf
caowanyang <zkcaowanyang@gmail.com>
2016-12-18 11:54:03 +0800

    struts2-portfolio

---
+++ Portfolio/WebContent/menu/Menu.jsp
@@ -0,0 +1,23 @@
+<%@ page contentType="text/html; charset=UTF-8" %>
+<%@ taglib prefix="s" uri="/struts-tags" %>
+<html>
+	<head>
+    <title>Struts 2 in Action: Menu</title>
+	</head>
+
+	<body>
+	
+	  <!-- The property struts tag reads the value off the value stack.  In our case
+	       the Struts 2 framework has automatically made the Java bean properties of
+	       our action available to the result.  We just need to match the nomenclature
+	       of our value paramater below with the Java Beans attribute name of our 
+	       HelloWorld action.  
+	  -->      
+  <hr>   
+  <ul>
+     <li><a href="<s:url action='manning/PortfolioHomePage'/>">Struts 2 Portfolio (Chapter 3)</a></li>
+ 	</ul>
+  <hr>   	
+	</body>
+	
+</html>
---
+++ Portfolio/src/manning/modelDriven/ModelDrivenRegister.java
@@ -0,0 +1,63 @@
+package manning.modelDriven;
+
+import manning.utils.PortfolioService;
+import manning.utils.User;
+
+import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.ModelDriven;
+
+/*
+ * This is our ModelDriven version of the Register action.  This action
+ * implements the ModelDriven interface, which exposes the single getModel()
+ * method which is used by the model driven interceptor to retrieve our 
+ * User object for data transfer.  
+ *   
+ */
+
+public class ModelDrivenRegister extends ActionSupport implements ModelDriven {
+
+	public String execute() {
+
+		getPortfolioService().createAccount(user);
+
+		return SUCCESS;
+	}
+
+	/* Instance field and getModel() to support the ModelDriven interface */
+
+	private User user = new User();
+
+	public Object getModel() {
+
+		return user;
+	}
+
+	public void validate() {
+
+		/* Retrieve our simple portfolio service object. */
+		PortfolioService ps = getPortfolioService();
+
+		/* Check that fields are not empty */
+		if (user.getPassword().length() == 0) {
+			addFieldError("password", getText("password.required"));
+		}
+		if (user.getUsername().length() == 0) {
+			addFieldError("username", getText("username.required"));
+		}
+		if (user.getPortfolioName().length() == 0) {
+			addFieldError("portfolioName", getText("portfolioName.required"));
+		}
+		/* Make sure user doesn't already have an account */
+		if (ps.userExists(user.getUsername())) {
+			addFieldError("username", getText("user.exists"));
+		}
+
+	}
+
+	public PortfolioService getPortfolioService() {
+
+		return new PortfolioService();
+
+	}
+
+}
